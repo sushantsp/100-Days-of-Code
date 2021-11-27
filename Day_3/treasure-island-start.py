1514bd39a267194e0bfd08ed95bd4c84eaf28017
Sushant <32874962+sushantsp@users.noreply.github.com>
2021-11-27 14:13:52 +0530

    Update treasure-island-start.py
    new line added

--- Day_3/treasure-island-start.py
+++ Day_3/treasure-island-start.py
@@ -26,11 +26,11 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-cross_road = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"').lower()
+cross_road = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"\n').lower()
 if cross_road == 'left':
-  lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+  lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
   if lake == "wait":
-    ryb = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
+    ryb = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
     if ryb == "red":
       print("It's a room full of fire. Game Over.")
     elif ryb == "yellow":
