6627872db7ef3f8e42c52dc9eda3e991321c472f
Sushant <32874962+sushantsp@users.noreply.github.com>
2021-11-28 09:35:49 +0530

    Create Heads or Tails.py

---
+++ Day_4/Heads or Tails.py
@@ -0,0 +1,17 @@
+#Remember to use the random module
+#Hint: Remember to import the random module first. 🎲
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write your code below this line 👇
+
+random_int = round(random.randint(0,1))
+
+if random_int == 0:
+    print("Tails")
+else:
+    print('Heads')
+
