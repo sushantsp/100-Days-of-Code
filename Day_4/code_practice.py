15e0e3b6023357b928affe86e913796550bdbd68
Sushant <32874962+sushantsp@users.noreply.github.com>
2021-11-28 10:34:43 +0530

    Create code_practice.py

---
+++ Day_4/code_practice.py
@@ -0,0 +1,23 @@
+import random
+
+random_integer = random.randint(1,10)
+random_float = random.random()
+
+print(random_integer)
+print(random_float)
+
+# Lists
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", 
+                     "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", 
+                     "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", 
+                     "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", 
+                     "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", 
+                     "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", 
+                     "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+state_of_america[1] = "pencilvenia"
+
+states_of_america.append("kite land")
+states_of_america.extend(['timeswuqre', 'adjlkf'])
+
+#https://docs.python.org/3/tutorial/datastructures.html
