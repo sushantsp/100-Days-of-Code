069a301eff5b80083b29d0ae53ce1b0d425df056
Sushant <32874962+sushantsp@users.noreply.github.com>
2021-12-01 13:45:15 +0530

    Update 0_python_codes.py

--- Day_6/0_python_codes.py
+++ Day_6/0_python_codes.py
@@ -1,3 +1,4 @@
+# First challenge
 def turn_around():
     turn_left()
     turn_left()
@@ -13,3 +14,27 @@ turn_right()
 move()
 turn_right()
 move()
+
+# Second challenge
+
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_ahead():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+  
+for i in range(6):
+    move_ahead()
+
