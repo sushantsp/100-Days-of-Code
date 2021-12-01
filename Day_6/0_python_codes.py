cadeb6c4435690bbf8053f6ebed99e00e590c579
Sushant <32874962+sushantsp@users.noreply.github.com>
2021-12-01 19:44:11 +0530

    Create 0_python_codes.py

--- Day_6/0_python_codes.py
+++ Day_6/0_python_codes.py
@@ -38,3 +38,34 @@ def move_ahead():
 for i in range(6):
     move_ahead()
 
+
+    
+    
+# 4th Hurdle Challenge :
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def move_ahead():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        move_ahead()
+    elif not wall_in_front():
+        move()
+
+        
+
+
